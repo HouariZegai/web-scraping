00d733151f4b52d8868bf5acea7028d9c8880993
HouariZegai <houarizegai14@gmail.com>
2020-04-29 01:36:59 +0100

    Amazon - scrape the book names

---
+++ src/main/java/com/houarizegai/webscraping/amazon/AmazonBookScraper.java
@@ -0,0 +1,26 @@
+package com.houarizegai.webscraping.amazon;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AmazonBookScraper {
+    public static void main(String[] args) throws IOException {
+            String url = "https://www.amazon.com/Best-Sellers-Kindle-Store-Computer-Programming/zgbs/digital-text/156140011";
+        Document page = Jsoup.connect(url).userAgent("JSoup scraper").get();
+
+        String bookTitleSelector = "p13n-sc-truncate";
+        // Get the books name using the html class selector
+        Elements bookElements = page.getElementsByClass(bookTitleSelector);
+
+        List<String> booksTitle = bookElements.stream()
+                .map(element -> element.text())
+                .collect(Collectors.toList());
+
+        booksTitle.forEach(System.out::println);
+    }
+}
